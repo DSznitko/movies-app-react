adc7c7c982000cae1218c05c4fcb314f8e782694
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2022-08-21 20:43:47 +0200

    create cookie popup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Slider from "./components/slides/Slider";
+import CookiesPopup from "./components/cookie/CookiesPopup";
 
 function App() {
+  const [cookieActive, setCookieActive] = useState(true);
   return (
     <div className="App">
       <Header />
       <Slider />
+      {cookieActive && (
+        <CookiesPopup
+          cookieActive={cookieActive}
+          setCookieActive={setCookieActive}
+        />
+      )}
     </div>
   );
 }
