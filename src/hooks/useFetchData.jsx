34358392fd56fa4205ca1ed61b078ca06f858fcc
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2022-10-13 20:07:18 +0200

    remove unnecesary component and finish usefetch custom hook

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFetchData = (url) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("Something went wrong");
+  const [error, setError] = useState(null);
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(url)
+      .then((res) => {
+        const data = res.data;
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        isLoading(false);
+      });
+  }, [url]);
+
+  return { isLoading, error, data };
 };
 
 export default useFetchData;
