e91c349d2005e4c0b3e721c394684730ac9f4476
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2022-11-06 14:04:10 +0100

    modify slider component create file .env store api key

--- src/components/slides/Slider.jsx
+++ src/components/slides/Slider.jsx
@@ -3,8 +3,8 @@ import classes from "./Slider.module.css";
 
 const Slider = () => {
   return (
-    <div className={classes.slider_container}>
-      <h2 className={classes.slider_title}>
+    <div className={classes.slider__container}>
+      <h2 className={classes.slider__title}>
         Top Rated <span className={classes.thin}>Movies</span>
       </h2>
       <ul className={classes.slides}></ul>
